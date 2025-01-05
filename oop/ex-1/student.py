16f25a3cbe95e993e7389da5ed447b5de2729b74
HariharanThavaa <31152785+HariharanThavaa@users.noreply.github.com>
2025-01-05 20:12:36 +0000

    Before Class

--- oop/ex-1/student.py
+++ oop/ex-1/student.py
@@ -1,3 +1,15 @@
-name = input("Name: ")
-house = input("House: ")
-print(f"{name}")
+def main():
+    name = get_name()
+    house = get_address()
+    print(f"{name} from {house}")
+
+def get_name():
+    name = input("Name: ")
+    return name
+
+def get_address():
+    house = input("House: ")
+    return house
+
+if __name__ == "__main__":
+    main()
