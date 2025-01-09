93ba056d3e0a15266c4c298f95c0d09ed6666f88
HariharanThavaa <31152785+HariharanThavaa@users.noreply.github.com>
2025-01-09 22:20:03 +0000

    Applying class method in student.py

--- oop/ex-1/student.py
+++ oop/ex-1/student.py
@@ -6,36 +6,15 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    @property
-    def house(self):
-        return self._house
-    
-    @property
-    def name(self):
-        return self._name
-
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house") 
-        self._house = house
-    
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Missing name")   
-        self._name = name
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
 
 
 def main():
-    student = get_student()
-    student._house = "Homeless"
-    print(student)
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house)
+    print(Student.get())
 
 if __name__ == "__main__":
     main()
