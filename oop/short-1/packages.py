aaf267312e4fe0a1c9dcbdf1ae3659f6a9a5cd9c
HariharanThavaa <31152785+HariharanThavaa@users.noreply.github.com>
2025-01-12 20:19:12 +0000

    hello.py

--- oop/short-1/packages.py
+++ oop/short-1/packages.py
@@ -8,12 +8,16 @@ class Package:
     def __str__(self):
         return f"Package {self.number}: {self.sender} to {self.receiver}, {self.weight}kg"
 
+    def calculate_cost(self, cost_per_kg):
+        return self.weight * cost_per_kg
+
 def main():
     packages = [
         Package(number = 1, sender = "Alice", receiver = "Bob", weight = 10),
         Package(number = 2, sender = "Bob", receiver = "Charlie", weight = 5)
     ]
     for pkg in packages:
-        print(pkg)
+        print(f"{pkg.number}. {pkg}")
+        print(f"cost of Package {pkg.number} is {pkg.calculate_cost(cost_per_kg = 50)}")
 
 main()
