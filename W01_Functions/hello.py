c39c9a6b122b9e4bdbd67ccf929f2da2205ff504
HariharanThavaa <31152785+HariharanThavaa@users.noreply.github.com>
2025-01-13 00:01:47 +0000

    Using main

--- W01_Functions/hello.py
+++ W01_Functions/hello.py
@@ -1,6 +1,9 @@
+def main():
+    name = input("What's you name? ")
+    hello(name)
+
 def hello(to="World"):
     print("Hello,", f"{to}!")
 
-hello()
-name = input("What's you name? ")
-hello(name)
+main()
+
